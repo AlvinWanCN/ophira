5ac2f19aabff9414366023105837d33a5b7fe424
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-05-08 10:54:22 +0800

    update

--- sophiroth/views.py
+++ sophiroth/views.py
@@ -188,7 +188,7 @@ def update_code_api(request):
     try:
         if request.method == 'GET':
             os.chdir('/home/alvin/ophira')
-            subprocess.call('/usr/bin/git pull')
+            subprocess.call('/usr/bin/git pull', shell=True)
             return JsonResponse({'success': True,'code':0,'message':'已更新'})
         else:
             return JsonResponse({'success': False, 'code': 1, 'message': '请使用POST请求'})
