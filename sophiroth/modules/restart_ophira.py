bc2a9d8eda8e24440bac50566d633ec45504060c
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-05-09 10:59:39 +0800

    update

--- sophiroth/modules/restart_ophira.py
+++ sophiroth/modules/restart_ophira.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #coding:utf-8
-import subprocess
+import subprocess,time
 
 subprocess.call("kill -9 `ps -eo pid,cmd|grep /usr/bin/python|grep runserver|grep 8003|awk '{print $1}'`", shell=True)
+time.sleep(2)
 subprocess.call('nohup python manage.py runserver 0.0.0.0:8003 &>/tmp/8003.log &', shell=True)
