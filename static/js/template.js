a9aab79c8b35d49d529502133dacf20169ec7723
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-05-11 15:05:56 +0800

    update

--- static/js/template.js
+++ static/js/template.js
@@ -1,8 +1,28 @@
 /**
  * Created by Alvin Wan on 4/22/2018.
  */
-function now_time(){
-    var nowtime = new Date()
-    document.write(nowtime)
+// function now_time(){
+//     var nowtime = new Date()
+//     document.write(nowtime)
+// }
+// now_time()
+
+Date.prototype.format = function(format)
+{
+ var o = {
+ "M+" : this.getMonth()+1, //month
+ "d+" : this.getDate(),    //day
+ "h+" : this.getHours(),   //hour
+ "m+" : this.getMinutes(), //minute
+ "s+" : this.getSeconds(), //second
+ "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+ "S" : this.getMilliseconds() //millisecond
+ }
+ if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
+ (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+ for(var k in o)if(new RegExp("("+ k +")").test(format))
+ format = format.replace(RegExp.$1,
+ RegExp.$1.length==1 ? o[k] :
+ ("00"+ o[k]).substr((""+ o[k]).length));
+ return format;
 }
-now_time()
